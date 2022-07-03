8ae8583b4bbd1924797bd15fabf2cdcf4e26fc36
Andrey Kudinov <2499931@gmail.com>
2022-07-03 21:42:31 +0400

    Add logic draft

--- main.js
+++ main.js
@@ -1,3 +1,79 @@
+const data = [
+  {
+    title: 'Node 1',
+    nodes: [
+      {
+        title: 'Node 1.1',
+        nodes: [
+          {
+            title: 'Node 1.1.1',
+            nodes: [
+              {
+                title: 'Node 1.1.1.1',
+                nodes: null
+              },
+              {
+                title: 'Node 1.1.1.2',
+                nodes: null
+              },
+              {
+                title: 'Node 1.1.1.3',
+                nodes: null
+              },
+              {
+                title: 'Node 1.1.1.4',
+                nodes: null
+              }
+            ]
+          }
+        ]
+      },
+      {
+        title: 'Node 1.2',
+        nodes: [
+          {
+            title: 'Node 1.2.1',
+            nodes: null
+          },
+          {
+            title: 'Node 1.2.2',
+            nodes: null
+          },
+          {
+            title: 'Node 1.2.3',
+            nodes: null
+          },
+          {
+            title: 'Node 1.2.4',
+            nodes: null
+          }
+        ]
+      }
+    ]
+  }
+]
 
+const createNode = (nodes, box) => {
+  if (nodes?.length) {
+    const container = document.createElement('div')
+    container.classList.add('container')
 
-console.log(1)
+    nodes.forEach(node => {
+      const nodeElement = document.createElement('div')
+      nodeElement.innerHTML = `
+        <div class='card'>
+          <div class='header'><h2>${node.title}</h2></div>
+          <div class='body'>body</div>
+        <div/>
+      `
+
+      createNode(node.nodes, container)
+      box ? box.append(nodeElement) : document.body.append(nodeElement)
+    })
+    if (box) {
+      document.body.append(box)
+    }
+  }
+}
+
+createNode(data)
