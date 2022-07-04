3b1ffcc09634ee592b7f67e1dcb1e721f6f57891
Andrey Kudinov <2499931@gmail.com>
2022-07-04 12:19:12 +0400

    Add logic for d3 js

--- main.js
+++ main.js
@@ -53,27 +53,76 @@ const data = [
   }
 ]
 
-const createNode = (nodes, box) => {
-  if (nodes?.length) {
-    const container = document.createElement('div')
-    container.classList.add('container')
+// set the dimensions and margins of the diagram
+const margin = { top: 20, right: 90, bottom: 30, left: 90 },
+  width = 660 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom
 
-    nodes.forEach(node => {
-      const nodeElement = document.createElement('div')
-      nodeElement.innerHTML = `
-        <div class='card'>
-          <div class='header'><h2>${node.title}</h2></div>
-          <div class='body'>body</div>
-        <div/>
-      `
+// declares a tree layout and assigns the size
+const treemap = d3.tree().size([height, width])
 
-      createNode(node.nodes, container)
-      box ? box.append(nodeElement) : document.body.append(nodeElement)
-    })
-    if (box) {
-      document.body.append(box)
-    }
-  }
-}
+//  assigns the data to a hierarchy using parent-child relationships
+let nodes = d3.hierarchy(data[0], d => d.nodes)
 
-createNode(data)
+// maps the node data to the tree layout
+nodes = treemap(nodes)
+
+// append the svg object to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+const svg = d3
+    .select('body')
+    .append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom),
+  g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+// adds the links between the nodes
+const link = g
+  .selectAll('.link')
+  .data(nodes.descendants().slice(1))
+  .enter()
+  .append('path')
+  .attr('class', 'link')
+  .style('stroke', 'orange')
+  .attr('d', d => {
+    return (
+      'M' +
+      d.y +
+      ',' +
+      d.x +
+      'C' +
+      (d.y + d.parent.y) / 2 +
+      ',' +
+      d.x +
+      ' ' +
+      (d.y + d.parent.y) / 2 +
+      ',' +
+      d.parent.x +
+      ' ' +
+      d.parent.y +
+      ',' +
+      d.parent.x
+    )
+  })
+
+// adds each node as a group
+const node = g
+  .selectAll('.node')
+  .data(nodes.descendants())
+  .enter()
+  .append('g')
+  .attr('class', d => 'node' + (d.children ? ' node--internal' : ' node--leaf'))
+  .attr('transform', d => 'translate(' + d.y + ',' + d.x + ')')
+
+// adds the circle to the node
+node.append('circle').attr('r', 7.5).style('stroke', 'grey').style('fill', 'green')
+
+// adds the text to the node
+node
+  .append('text')
+  .attr('dy', '.35em')
+  .attr('x', d => (d.nodes ? (7.5 + 5) * -1 : 7.5 + 5))
+  .attr('y', d => (d.nodes && d.depth !== 0 ? -(7.5 + 5) : d))
+  .style('text-anchor', d => (d.nodes ? 'end' : 'start'))
+  .text(d => d.data.title)
